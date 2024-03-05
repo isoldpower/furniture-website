8870dab12953656ae7e7e62b195769bfdaf07a25
isoldpower <Lapin_NR@icloud.com>
2024-03-06 00:06:13 +0500

    feat: More-info Link

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,15 @@
 import '@/app/scss/main.scss';
 import classes from './Home.module.scss';
 import {CallbackSection} from "@/widgets/callback-section/ui/CallbackSection";
+import {MoreLink} from "@/shared/ui/more-link/ui/MoreLink";
 
 const Home = () => {
     return (
         <div className={`${classes.homePage__wrapper}`}>
             <div className={`${classes.homePage__content}`}>
-                <section className={`${classes.homePage__callbackSection}`}>
+                <div className={`${classes.homePage__callbackSection}`}>
                     <CallbackSection />
-                </section>
+                </div>
             </div>
         </div>
     );
---
+++ src/shared/ui/more-link/ui/MoreLink.tsx
@@ -0,0 +1,21 @@
+import {BaseHTMLAttributes, FC} from "react";
+import '@/app/scss/main.scss';
+import classes from './MoreLink.module.scss';
+import {Link, LinkProps} from "react-router-dom";
+import {Reference} from "@/shared/icons";
+
+interface MoreLinkProps extends LinkProps {
+}
+
+export const MoreLink: FC<MoreLinkProps> = ({className, children, ...props}: MoreLinkProps) => {
+    return (
+        <Link className={`${classes.moreLink__wrapper} ${className}`} {...props}>
+            <div className={`${classes.moreLink__content} cc-flex cc-width-1of1 cc-align-items-center cc-justify-content-center cc-gap-1`} tabIndex={0}>
+                {children}
+                <span className={`${classes.moreLink__arrowWrapper}`}>
+                    <Reference  className={`${classes.moreLink__arrow}`} width={16} height={16}/>
+                </span>
+            </div>
+        </Link>
+    );
+};
