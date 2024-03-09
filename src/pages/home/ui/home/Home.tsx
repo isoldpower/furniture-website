fd9b99ba7189da26c04c5ae1c321ae786bc476b3
isoldpower <Lapin_NR@icloud.com>
2024-03-10 00:32:45 +0500

    fix: linting fix
    decomposition of hero section; stylistic changes in order of attributes

--- src/pages/home/ui/home/Home.tsx
+++ src/pages/home/ui/home/Home.tsx
@@ -1,9 +1,9 @@
 import '@/app/scss/main.scss';
 import classes from './Home.module.scss';
 import {CallbackSection} from "@/widgets/callback-section/ui/CallbackSection";
-import {Button} from "@/shared/ui";
 import {HeroDecorationsProvider} from "@/pages/home/ui/hero-decorations/HeroDecorationsProvider";
 import {BaseHTMLAttributes, FC} from "react";
+import {HeroSection} from "@/pages/home/ui/hero-section/HeroSection";
 
 interface HomeProps extends BaseHTMLAttributes<HTMLDivElement> {
 }
@@ -14,22 +14,8 @@ export const Home: FC<HomeProps> = ({className, ...props}: HomeProps) => {
             <div className={`${classes.homePage__content} cc-grid cc-gap-15 cc-laptop-gap-18`}>
                 <div className={`${classes.homePage__heroWrapper}`}>
                     <section className={`${classes.homePage__heroSection} cc-main-gutter`}>
-                        <HeroDecorationsProvider>
-                            <div className={`${classes.heroSection__content} cc-gap-13 cc-main-gutter-content`}>
-                                <div className={`${classes.heroSection__body} cc-gap-10`}>
-                                    <h1 className={`${classes.heroSection__heading} cc-heading-1`}>Корпусная
-                                        мебель <br/> под заказ</h1>
-                                    <p className={`${classes.heroSection__paragraph}`}>Спроектируем и сделать мебель с
-                                        точностью как вы хотите, учитывая все ваши пожелания с использованием качественных
-                                        отделочных материалов</p>
-                                </div>
-                                <div className={`${classes.heroSection__buttons}`}>
-                                    <Button semantic={'link'} to={'/catalog'} data={{variant: 'accent'}}
-                                            className={`${classes.heroSection__accentButton}`}>Перейти в каталог</Button>
-                                    <Button semantic={'link'} to={'/about'} className={`${classes.heroSection__aboutButton}`}>Подробнее
-                                        о компании</Button>
-                                </div>
-                            </div>
+                        <HeroDecorationsProvider className="cc-width-1of1">
+                            <HeroSection/>
                         </HeroDecorationsProvider>
                     </section>
                 </div>
