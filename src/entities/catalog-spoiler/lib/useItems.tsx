b25ecf06a71e553aa6b74bc71330ad622e013a89
isoldpower <Lapin_NR@icloud.com>
2024-03-12 02:27:48 +0500

    feat: featured section

---
+++ src/entities/catalog-spoiler/lib/useItems.tsx
@@ -0,0 +1,15 @@
+import {ProductData} from "@/entities/product/model";
+import {DetailedProductCard} from "@/widgets/detailed-product-card/ui/DetailedProductCard";
+import classes from "@/entities/catalog-spoiler/ui/CatalogSpoiler.module.scss";
+import {breakpoints, useDocumentSize} from "@/shared/lib";
+
+export const placeItems = (products: ProductData[]) => {
+    const width = useDocumentSize().x;
+    const amount = width >= breakpoints.desktop ? 3 : 2;
+
+    return products.map((product, key) => {
+        return key + 1 <= amount
+            ? <DetailedProductCard className={`${classes.catalogSpoiler__item} ${classes.important}`} data={product} key={key}/>
+            : <div className={`${classes.catalogSpoiler__invisible}`} key={key} />
+    })
+}
