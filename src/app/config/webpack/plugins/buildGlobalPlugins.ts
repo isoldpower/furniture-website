2e60771823dc20a67c507ed2e20d04939eecd1ce
isoldpower <Lapin_NR@icloud.com>
2024-04-04 02:43:56 +0500

    fix: favicon fix

--- src/app/config/webpack/plugins/buildGlobalPlugins.ts
+++ src/app/config/webpack/plugins/buildGlobalPlugins.ts
@@ -1,13 +1,11 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {Configuration} from "webpack";
 import {BuildOptions} from "../types";
-import path from "path";
 
 export function buildGlobalPlugins(options: BuildOptions): Configuration['plugins'] {
     return [
         new HtmlWebpackPlugin({
-            template: options.paths.html,
-            favicon: path.resolve(options.paths.public, 'favicon.ico')
+            template: options.paths.html
         })
     ]
 }
