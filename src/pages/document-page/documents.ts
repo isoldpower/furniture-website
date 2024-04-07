6fb00a90f0d593768d11406949d1520eb257a393
isoldpower <Lapin_NR@icloud.com>
2024-04-07 21:56:38 +0500

    refactor: documents folder refactor

---
+++ src/pages/document-page/documents.ts
@@ -0,0 +1,12 @@
+import {Document} from "@/pages/document-page/model";
+
+export const documents: {[name: string]: Document} = {
+    policy: {
+        download: 'no-download',
+        document: 'no-document'
+    },
+    agreement: {
+        download: 'no-download',
+        document: 'no-document'
+    }
+}
